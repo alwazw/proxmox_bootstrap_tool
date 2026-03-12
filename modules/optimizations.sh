b1ca65ea51798e5deecfbdfadd9ba5aebdea858e
ALWAZW <99280338+alwazw@users.noreply.github.com>
2026-03-12 06:03:39 -0400

    Add message for system tuning completion

--- modules/optimizations.sh
+++ modules/optimizations.sh
@@ -1,7 +1,7 @@
-msg_ok "Applying ZFS and Network Tuning"
 echo "options zfs zfs_arc_max=8589934592" > /etc/modprobe.d/zfs.conf
 cat <<SYS >/etc/sysctl.d/99-proxmox.conf
 net.core.rmem_max=134217728
 net.core.wmem_max=134217728
 SYS
 sysctl -p /etc/sysctl.d/99-proxmox.conf &>/dev/null
+msg_ok "SYSTEM TUNED"
