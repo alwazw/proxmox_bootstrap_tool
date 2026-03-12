fa6bbc1bfd8eef729aa654f233ba0bdbb5ba746c
ALWAZW <99280338+alwazw@users.noreply.github.com>
2026-03-12 09:51:47 -0400

    Delete modules/optimizations.sh

--- modules/optimizations.sh
+++
@@ -1,7 +0,0 @@
-echo "options zfs zfs_arc_max=8589934592" > /etc/modprobe.d/zfs.conf
-cat <<SYS >/etc/sysctl.d/99-proxmox.conf
-net.core.rmem_max=134217728
-net.core.wmem_max=134217728
-SYS
-sysctl -p /etc/sysctl.d/99-proxmox.conf &>/dev/null
-msg_ok "SYSTEM TUNED"
