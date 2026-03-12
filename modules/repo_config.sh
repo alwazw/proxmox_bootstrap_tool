8270232660ddb8605fa4f1501d72a0db2875f3b8
ALWAZW <99280338+alwazw@users.noreply.github.com>
2026-03-12 06:04:13 -0400

    Update message for migrating to DEB822 repositories

--- modules/repo_config.sh
+++ modules/repo_config.sh
@@ -1,12 +1,9 @@
-msg_ok "Configuring DEB822 Repositories"
+msg_ok "MIGRATING TO DEB822 REPOSITORIES"
 KEYRING="/etc/apt/keyrings/proxmox-release-trixie.gpg"
 mkdir -p /etc/apt/keyrings
 wget -q https://enterprise.proxmox.com/debian/proxmox-release-trixie.gpg -O "$KEYRING"
-
-# Clean legacy .list files
 mkdir -p /root/pve_repo_backup
 mv /etc/apt/sources.list.d/*.list /root/pve_repo_backup/ 2>/dev/null || true
-
 cat <<REPOS > /etc/apt/sources.list.d/pve.sources
 Types: deb
 URIs: http://download.proxmox.com/debian/pve http://download.proxmox.com/debian/ceph-squid
