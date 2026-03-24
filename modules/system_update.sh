4fe7b4e78cb7f5748660ac124f33914fdac18bb6
ALWAZW <99280338+alwazw@users.noreply.github.com>
2026-03-24 04:08:56 -0400

    Update system_update.sh
    Signed-off-by: ALWAZW <99280338+alwazw@users.noreply.github.com>

--- modules/system_update.sh
+++ modules/system_update.sh
@@ -1,2 +1,9 @@
+# Define the msg_ok function
+msg_ok() {
+  local TEXT="$1"
+  echo -e " \e[32m\u2713\e[0m \e[32m$TEXT\e[0m"
+}
+
+
 msg_ok "Updating System & Installing Tools"
 apt-get update && apt-get full-upgrade -y &>/dev/null
