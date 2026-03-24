2b631c526fc545e2b4789c39077fc1d1e246f5ce
ALWAZW <99280338+alwazw@users.noreply.github.com>
2026-03-24 04:19:25 -0400

    Update tmux_setup.sh
    Signed-off-by: ALWAZW <99280338+alwazw@users.noreply.github.com>

--- modules/tmux_setup.sh
+++ modules/tmux_setup.sh
@@ -1,9 +1,8 @@
 apt-get install -y tmux &>/dev/null
 if ! grep -q "tmux attach" /root/.bashrc; then
 cat <<TMUX >> /root/.bashrc
- if command -v tmux &> /dev/null && [ -z "\$TMUX" ]; then
-    tmux attach -t main || tmux new -s main
-fi
-TMUX
+ if [ -f ~/.bashrc ]; then
+     . ~/.bashrc
+ fi
 fi
 msg_ok "TMUX CONFIGURED"
