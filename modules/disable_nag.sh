2f9264d92c54ae71fbee9baf4a0bb1aa373d7899
ALWAZW <99280338+alwazw@users.noreply.github.com>
2026-03-24 04:07:00 -0400

    Update disable_nag.sh
    Signed-off-by: ALWAZW <99280338+alwazw@users.noreply.github.com>

--- modules/disable_nag.sh
+++ modules/disable_nag.sh
@@ -1,3 +1,10 @@
+# Define the msg_ok function
+msg_ok() {
+  local TEXT="$1"
+  echo -e " \e[32m\u2713\e[0m \e[32m$TEXT\e[0m"
+}
+
+
 sed -i 's/data.status !== "Active"/false/g' /usr/share/javascript/proxmox-widget-toolkit/proxmoxlib.js
 systemctl restart pveproxy &>/dev/null
 msg_ok "NAG REMOVED"
